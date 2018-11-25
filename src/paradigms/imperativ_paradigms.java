61989199d0736f4d4e51cb39abbca8d61f06ec2e
xhuljana <xhulihima@hotmail.it>
2018-11-25 16:28:28 +0100

    Imperative

---
+++ src/paradigms/imperativ_paradigms.java
@@ -0,0 +1,14 @@
+package paradigms;
+
+public class imperativ_paradigms {
+	public static void main (String args[]) {
+		int num = 15;
+		int counter = 0;
+		while (counter <10) {
+			num = num + 1;
+			counter ++;
+		}
+		System.out.println("Sum is "+num);
+	}
+
+}
